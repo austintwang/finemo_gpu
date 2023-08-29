8faf003f5952108fa0e10cfc15242d9f98f2c452
Austin <austin.wang1357@gmail.com>
2023-08-29 08:44:55 -0700

    Debug floating point precision

--- finemo/hitcaller.py
+++ finemo/hitcaller.py
@@ -17,11 +17,11 @@ def log_likelihood(coefficients, cwms_t, contribs, sequences):
     pred_masked = pred * sequences # (b, 4, l)
 
     ll = F.mse_loss(pred_masked, contribs, reduction='none').sum(dim=(1,2))
-    print(ll) ####
+    # print(ll) ####
     # print(pred_masked) ####
     # print(contribs) ####
-    print(F.mse_loss(pred_masked, contribs, reduction='none')) ####
-    print(((pred_masked - contribs)**2).sum(dim=(1,2))) ####
+    # print(F.mse_loss(pred_masked, contribs, reduction='none')) ####
+    # print(((pred_masked - contribs)**2).sum(dim=(1,2))) ####
 
     
     return ll, pred_masked
