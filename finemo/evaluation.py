db7ec85c91bf87ee4ee0011454ba98995c4dbbea
Austin <austin.wang1357@gmail.com>
2023-10-09 13:06:48 -0700

    Recall plots

--- finemo/evaluation.py
+++ finemo/evaluation.py
@@ -389,7 +389,8 @@ def plot_frac_peaks(occ_bin, motif_names, plot_path):
 def plot_modisco_recall(seqlet_recalls, plot_dir):
     os.makedirs(plot_dir, exist_ok=True)
 
-    labels = sorted(seqlet_recalls.keys(), key=lambda x: (int(x.split("_")[-1]), x.split("_")[0] == "neg"))
+    labels = sorted(seqlet_recalls.keys(), 
+                    key=lambda x: (x.split("_")[0] == "neg", int(x.split("_")[-1])), reverse=True)
     recalls = [seqlet_recalls[l][-1] for l in labels]
 
     fig, ax = plt.subplots(figsize=(12, 8))
@@ -409,6 +410,7 @@ def plot_modisco_recall(seqlet_recalls, plot_dir):
         plt.xlabel("Hit rank")
         plt.ylabel("Recall")
         
+        plt.tight_layout()
         output_path = os.path.join(plot_dir, f"{k}.png")
         plt.savefig(output_path, dpi=300)
 
